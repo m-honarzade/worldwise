f10035eae61c4087bb0ce2c78a36946e4ac69fe2
m-honarzade <honarzadeh.mahdie@gmail.com>
2024-04-06 17:16:56 +0330

    adding protecting route functionality

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import City from "./components/app/City";
 import Form from "./components/form/Form";
 import { CitiesProvider, useCities } from "./contexts/CitiesContext";
 import { AuthProvider } from "./contexts/AuthFakeApiContext";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
@@ -24,7 +25,14 @@ function App() {
               <Route path="pricing" element={<Pricing />} />
               <Route path="product" element={<Product />} />
               <Route path="login" element={<Login />} />
-              <Route path="app" element={<AppLayout />}>
+              <Route
+                path="app"
+                element={
+                  <ProtectedRoute>
+                    <AppLayout />
+                  </ProtectedRoute>
+                }
+              >
                 <Route index element={<Navigate to="cities" replace />} />
                 <Route path="cities" element={<CityList />} />
                 <Route path="cities/:id" element={<City />} />
---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,14 @@
+import { useEffect } from "react";
+import { useAuth } from "../contexts/AuthFakeApiContext";
+import { useNavigate } from "react-router-dom";
+
+const ProtectedRoute = ({ children }) => {
+  const { isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!isAuthenticated) navigate("/");
+  }, [isAuthenticated, navigate]);
+  return isAuthenticated ? children : null;
+};
+
+export default ProtectedRoute;
